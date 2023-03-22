e00ebe0b7828cec117431cbc19fe55fa593809b3
wali55 <walisantunu@gmail.com>
2023-03-22 14:28:59 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Introducing our premium quality socks, designed to keep your feet comfortable and stylish all day long. Made from a soft, breathable material, these socks offer the perfect combination of comfort and durability. With a variety of colors and patterns to choose from, you can find the perfect pair to match your style. Ideal for both casual and formal occasions, these socks are a must-have for any wardrobe. Upgrade your sock game with our high-quality, affordable socks!'
+        }
+    }
+});
